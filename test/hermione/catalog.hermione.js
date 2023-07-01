612c24b975fba14404413d903b848c19f5952858
danlevik <leviol80@gmail.com>
2023-07-01 16:27:32 +0300

    initial setup and catalog tests

---
+++ test/hermione/catalog.hermione.js
@@ -0,0 +1,79 @@
+const { assert } = require("chai");
+const { async } = require("rxjs");
+
+describe("Если товар добавлен в корзину, информация об этом отражается в карточке и на странице", async function () {
+  it("Отображается надпись на странице товара", async function () {
+    await this.browser.url("http://localhost:3000/hw/store/catalog");
+    const cardLink = await this.browser.$("[data-testid='0'] .card-link");
+    await cardLink.click();
+    const addToCartButton = await this.browser.$(".ProductDetails-AddToCart");
+    await addToCartButton.click();
+    const cartBadge = await this.browser.$(".CartBadge").getText();
+
+    assert.ok(cartBadge, "Надпись не появилась");
+    assert.equal(cartBadge, "Item in cart");
+  });
+
+  it("Отображается надпись в каталоге", async function () {
+    await this.browser.url("http://localhost:3000/hw/store/catalog");
+    const cardLink = await this.browser.$("[data-testid='0'] .card-link");
+    await cardLink.click();
+    const addToCartButton = await this.browser.$(".ProductDetails-AddToCart");
+    await addToCartButton.click();
+    await this.browser.url("http://localhost:3000/hw/store/catalog");
+    const cartBadge = await this.browser
+      .$("[data-testid='0'] .CartBadge")
+      .getText();
+
+    assert.ok(cartBadge, "Надпись не появилась");
+    assert.equal(cartBadge, "Item in cart");
+  });
+});
+
+describe("Для каждого товара отображаются все данные", async function () {
+  it("Отображается название товара", async function () {
+    await this.browser.url("http://localhost:3000/hw/store/catalog");
+    const cardTitle = await this.browser
+      .$("[data-testid='0'] .ProductItem-Name")
+      .getText();
+
+    assert.isString(cardTitle);
+    assert.notEqual(cardTitle, "");
+  });
+  it("Отображается цена товара", async function () {
+    await this.browser.url("http://localhost:3000/hw/store/catalog");
+    const cardPrice = await this.browser
+      .$("[data-testid='0'] .ProductItem-Price")
+      .getText();
+
+    assert.isString(cardPrice);
+    assert.notEqual(cardPrice, "$");
+  });
+
+  it("Отображается ссылка на товар", async function () {
+    await this.browser.url("http://localhost:3000/hw/store/catalog");
+    const cardLink = await this.browser.$(
+      "[data-testid='0'] .ProductItem-DetailsLink"
+    );
+    const cardLinkText = await cardLink.getText();
+    const isLink = (await cardLink.getTagName("href")) == "a";
+
+    assert.equal(cardLinkText, "Details");
+    assert.isTrue(isLink);
+  });
+
+  it("Ссылка ведет на корректный товар", async function () {
+    await this.browser.url("http://localhost:3000/hw/store/catalog");
+
+    const cards = await this.browser.$$(".ProductItem");
+
+    await cards.forEach(async (element) => {
+      const dataId = await element.getAttribute("data-testid");
+      const linkElement = await element.$(".ProductItem-DetailsLink");
+      await expect(linkElement).toHaveAttribute(
+        "href",
+        `/hw/store/catalog/${dataId}`
+      );
+    });
+  });
+});
--- test/hermione/example.hermione.js
+++ test/hermione/example.hermione.js
@@ -1,11 +1,52 @@
-const { assert } = require('chai');
+const { assert } = require("chai");
+const { async } = require("rxjs");
 
-describe('microsoft', async function() {
-    it('Тест, который пройдет', async function() {
-        await this.browser.url('https://www.microsoft.com/ru-ru/');
-        await this.browser.assertView('plain', 'body');
+// describe("microsoft", async function () {
+//   it("Тест, который пройдет", async function () {
+//     await this.browser.url("https://www.microsoft.com/ru-ru/");
+//     await this.browser.assertView("plain", "header");
 
-        const title = await this.browser.$('#uhfLogo').getText();
-        assert.equal(title, 'Microsoft');
-    });
+//     const title = await this.browser.$("#uhfLogo").getText();
+//     assert.equal(title, "Microsoft");
+//   });
+// });
+
+describe("Название магазина в шапке должно быть ссылкой на главную страницу", async function () {
+  it("Элемент с названием магазина есть", async function () {
+    // await this.browser.url("http://localhost:3000/hw/store");
+    const puppeteer = await this.browser.getPuppeteer();
+    const [page] = await puppeteer.pages();
+    await page.goto("http://localhost:3000/hw/store");
+    await this.browser.pause(1000);
+
+    const titleElement = await page.$(".navbar-brand");
+    assert.ok(titleElement, "Элемента нет");
+    const titleText = await page.$eval(
+      ".navbar-brand",
+      (element) => element.innerHTML
+    );
+    assert.equal(titleText, "Example store");
+  });
+
+  it("Элемент - ссылка на главную страницу", async function () {
+    const puppeteer = await this.browser.getPuppeteer();
+    const [page] = await puppeteer.pages();
+    await page.goto("http://localhost:3000/hw/store");
+    await this.browser.pause(1000);
+
+    const href = await page.$eval(".navbar-brand", (element) =>
+      element.getAttribute("href")
+    );
+    assert.ok("href", "У элемента нет ссылки");
+    assert.equal(href, "/hw/store/");
+  });
+});
+
+// describe("В магазине должны быть все страницы", async function () {});
+
+describe("В шапке отображаются ссылки на страницы магазина, а также ссылка на корзину", async function () {
+  it("Все ссылки отображаются", async function () {
+    await this.browser.url("http://localhost:3000/hw/store");
+    await this.browser.assertView("correctLinks", "nav");
+  });
 });
