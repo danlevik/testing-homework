def356dd93b513386f661824290cd2da6b0a9ddd
danlevik <leviol80@gmail.com>
2023-07-01 20:58:49 +0300

    updated readme

--- test/hermione/cart.hermione.js
+++ test/hermione/cart.hermione.js
@@ -1,10 +1,13 @@
 const { assert } = require("chai");
 const { async } = require("rxjs");
 
-describe("В корзине должна отображаться таблица с добавленными в нее товарами", async function () {
+describe("В пустой корзине корректно отображаются сообщения и ссылки", async function () {
   it("Корректное сообщение, если в корзине нет товаров", async function () {
     await this.browser.url("http://localhost:3000/hw/store/cart");
     await this.browser.assertView("emptyCart", "body");
+
+    const linkToCatalog = await this.browser.$(".Cart a");
+    await expect(linkToCatalog).toHaveAttribute("href", "/hw/store/catalog");
   });
 
   it("Корректное сообщение, если был оформлен закан", async function () {
@@ -24,6 +27,15 @@ describe("В корзине должна отображаться таблица
 
     await this.browser.$(".Form-Submit").click();
 
-    await this.browser.assertView("successfullOrder", "body");
+    await this.browser.assertView("successfullOrder", "body", {
+      ignoreElements: [".Cart-SuccessMessage p"],
+    });
+
+    const linkToCatalog = await this.browser.$(".Cart a");
+    await expect(linkToCatalog).toHaveAttribute("href", "/hw/store/catalog");
   });
+});
+
+describe("Таблица с добавленными товарами в корзине корректно отображается", function () {
+  it("", async function () {});
 });
